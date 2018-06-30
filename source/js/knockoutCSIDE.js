5d1ba43e2fb75025916f169b97d6893003af090f
Carey Williams <careyjwilliams@gmail.com>
2018-06-30 01:03:27 +0100

    Odd indentation options + dropdown
    Provide the odd options for indentation (within the current range 2-8). Due to the amount of options after this change this commit also swaps the setting type to a dropdown menu.

--- source/js/knockoutCSIDE.js
+++ source/js/knockoutCSIDE.js
@@ -2817,17 +2817,26 @@ function IDEViewModel() {
         "id": "tabsize",
         "name": "Tab/Indent Block Size",
         "value": "4",
-        "type": "variable",
+        "type": "dropdown",
         "cat": "editor",
         "options": [{
           "desc": "2",
           "value": "2"
         }, {
+          "desc": "3",
+          "value": "3"
+        }, {
           "desc": "4",
           "value": "4"
         }, {
+          "desc": "5",
+          "value": "5"
+        }, {
           "desc": "6",
           "value": "6"
+        }, {
+          "desc": "7",
+          "value": "7"
         }, {
           "desc": "8",
           "value": "8"
