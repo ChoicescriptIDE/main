ChoicescriptIDE/main

commit 284737c77a66edc3897687c97a1df4bef26b23f5

[bug fix] Keep URIs in sync when renaming scenes
The internal Monaco services rely on Model URIs
to uniquely identify and keep track of models.
Unfortunately, there is no way to edit the Uri
of a model once it is created, which is problematic
when renaming scenes.

So what we're doing here is now replacing the model
on a rename. This works well enough, apart from one
really BIG caveat: it causes us to lose the
undo/redo stack. There might be ways to work around
this, but let's consider it a necessary regression
for now (see #115).

An alternative option would have been to use UIDs
for the URIs, over the file-path style, but this
would make it difficult on a lot of the
languageservice features such as the indexer.

source/js/knockoutCSIDE.js
@@ -478,8 +478,25 @@ function IDEViewModel() {
   function CSIDEScene(sceneData) {
     var self = this;
     //INSTANCE VARIABLES
+    var edModel; // holds the Monaco editor document model
+    self.dispose = function() { edModel.dispose(); };
     var path = ko.observable("").extend({
-      normalizePaths: ""
+      normalizePaths: "",
+      callFunc: {
+        func: function(newPath) {
+          // automatically replace the Monaco model on renames to keep its Uri in sync
+          var oldModel = edModel;
+          try {
+            edModel = monaco.editor.createModel(edModel ? edModel.getValue() : "", "choicescript", monaco.Uri.file(newPath));
+            edModel.csideScene = self;
+            if (oldModel) oldModel.dispose();
+            if (selectedScene() == self) vseditor.setModel(edModel);
+            edModel.onDidChangeContent(updateOnModelEdit);
+          } catch (err) {
+            bootbox.alert("Unrecoverable Error: Couldn't update URI for scene.<br><br>" + err.message + ".<br><br> Please restart the application and report this.");
+          }
+        }
+      }
     });
     path(sceneData.path);
     var name = ko.observable("").extend({
@@ -496,9 +513,6 @@ function IDEViewModel() {
     var saving = ko.observable(false);
     var inErrState = ko.observable(false);
     var errStateMsg = ko.observable("");
-    var edModel = monaco.editor.createModel(sceneData.contents || "", "choicescript", monaco.Uri.file(sceneData.path));
-    self.dispose = function() { edModel.dispose(); };
-    edModel.csideScene = self;
 
     // used for dirtyness
     var lastVersionId = edModel.getAlternativeVersionId();
@@ -1040,13 +1054,13 @@ function IDEViewModel() {
       }
 
     //Update dirty status, char count etc - on change
-    edModel.onDidChangeContent(function(e) {
+    function updateOnModelEdit() {
       if (!saving())
         lastVersionId !== edModel.getAlternativeVersionId() ? dirty(true) : dirty(false);
       charCount(edModel.getValueLength());
       if (wordCountOn() > 0)
         wordCount(__wordCount(edModel.getValue(), (wordCountOn() > 1)));
-    });
+    }
   }
 
   function csideIssue(issueData) {

commit b60996778612bae17c13bfc016581189b25ab506

[bug fix] Fix Monaco model creation on Windows
284737c moved model creation to be linked
with changes to the Scene path() observable.
This caused an issue on Windows because
the backslashed paths are normalized. This
is enough of a difference for the model creation
to be triggered twice, but not enough of a change
for Monaco to consider them separate URIs,
leading to an error.

This patch leaves the extended path() variable
to handle future changes, but moves initial model
creation back out. This also allowed me to fix
another accidental regression caused by 284737c
which was the loss of the default startup.txt
code.

source/js/knockoutCSIDE.js
@@ -480,7 +480,7 @@ function IDEViewModel() {
     //INSTANCE VARIABLES
     var edModel; // holds the Monaco editor document model
     self.dispose = function() { edModel.dispose(); };
-    var path = ko.observable("").extend({
+    var path = ko.observable(__normalizePath(sceneData.path)).extend({
       normalizePaths: "",
       callFunc: {
         func: function(newPath) {
@@ -498,7 +498,6 @@ function IDEViewModel() {
         }
       }
     });
-    path(sceneData.path);
     var name = ko.observable("").extend({
       lowerCase: ""
     });
@@ -513,6 +512,11 @@ function IDEViewModel() {
     var saving = ko.observable(false);
     var inErrState = ko.observable(false);
     var errStateMsg = ko.observable("");
+
+    // create initial model
+    var edModel = monaco.editor.createModel(sceneData.contents || "", "choicescript", monaco.Uri.file(path()));
+    edModel.onDidChangeContent(updateOnModelEdit);
+    edModel.csideScene = self;
 
     // used for dirtyness
     var lastVersionId = edModel.getAlternativeVersionId();