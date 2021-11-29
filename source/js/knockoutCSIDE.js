ChoicescriptIDE/main

commit 6384ec968a479fd68d2f0613a254cf17ae292bfd

Use numerical inputs for numerical settings
Switches the font size and tab+space size
settings to use a numerical input box,
which allows for free-form selection
from a wider range of values.

Value validation is still in place in
the form of a custome 'validate'
extender.

source/js/knockoutCSIDE.js
@@ -71,6 +71,21 @@ function IDEViewModel() {
     });
     return target;
   };
+  ko.extenders.validate = function(target, validateFunc) {
+    target.isErroneous = ko.observable();
+    target.errorMessage = ko.observable();
+
+    function doValidate(newValue) {
+      var result = validateFunc(target());
+      target.isErroneous(!result.valid);
+      target.errorMessage(result.message);
+    }
+
+    doValidate(target());
+    target.subscribe(doValidate);
+
+    return target;
+  };
 
   function __normalizePath(path) {
     //replace backslashes
@@ -1419,6 +1434,8 @@ function IDEViewModel() {
         "desc": "off",
         "value": false
       }]);
+    } else if (type === "number")  {
+      value.extend({ notify: 'always', validate: settingData.validate.bind(setting) });
     } else {
       var options = ko.observableArray(settingData.options);
     }
@@ -1440,7 +1457,7 @@ function IDEViewModel() {
       return cat;
     }
     setting.getOptions = ko.computed(function() {
-      return options();
+      return options ? options() : null;
     }, this);
     setting.getSelectedOptionId = function () {
       for (var i = 0; i < options().length; i++)
@@ -1483,9 +1500,15 @@ function IDEViewModel() {
     }
     value.subscribe(function(option) {
       config.settings[cat][id] = value(); //store
-      setting.apply(value());
-      __updateConfig(); //then write new settings object to localStorage
+      if (typeof value.isErroneous === "undefined" || !value.isErroneous()) {
+        setting.apply(value());
+        __updateConfig(); //then write new settings object to localStorage
+      }
     });
+
+    setting.max = settingData.max || null;
+    setting.min = settingData.min || null;
+    setting.step = settingData.step || null;
 
     setting.extAPI = {
       getName: setting.getName,
@@ -1495,7 +1518,10 @@ function IDEViewModel() {
       value: value,
       toggle: setting.toggle,
       getOptions: setting.getOptions,
-      isVisible: setting.isVisible
+      isVisible: setting.isVisible,
+      max: setting.max,
+      min: setting.min,
+      step: setting.step
     }
   }
 
@@ -2654,9 +2680,12 @@ function IDEViewModel() {
       keybindingContext: null,
       run: function(ed) {
         var fontSizeSetting = settings.byId("editor", "fontsize");
-        var optionId = fontSizeSetting.getSelectedOptionId();
-        if (typeof optionId == "number" && optionId < (fontSizeSetting.getOptions().length - 1))
-          fontSizeSetting.toggle(fontSizeSetting.getOptions()[optionId + 1]);
+        var value = fontSizeSetting.getValue();
+        if (value < fontSizeSetting.min) {
+          fontSizeSetting.setValue(fontSizeSetting.min);
+        } else if (value < fontSizeSetting.max) {
+          fontSizeSetting.setValue(++value);
+        }
         return null;
       }
     });
@@ -2671,9 +2700,12 @@ function IDEViewModel() {
       keybindingContext: null,
       run: function(ed) {
         var fontSizeSetting = settings.byId("editor", "fontsize");
-        var optionId = fontSizeSetting.getSelectedOptionId();
-        if (typeof optionId == "number" && optionId > 0)
-          fontSizeSetting.toggle(fontSizeSetting.getOptions()[optionId - 1]);
+        var value = fontSizeSetting.getValue();
+        if (value > fontSizeSetting.max) {
+          fontSizeSetting.setValue(fontSizeSetting.max);
+        } else if (value > fontSizeSetting.min) {
+          fontSizeSetting.setValue(--value);
+        }
         return null;
       }
     });
@@ -3211,84 +3243,81 @@ function IDEViewModel() {
         "id": "tabsize",
         "name": "Tab/Indent Block Size",
         "value": "4",
-        "type": "dropdown",
+        "type": "number",
         "cat": "editor",
-        "options": [{
-          "desc": "2",
-          "value": "2"
-        }, {
-          "desc": "3",
-          "value": "3"
-        }, {
-          "desc": "4",
-          "value": "4"
-        }, {
-          "desc": "5",
-          "value": "5"
-        }, {
-          "desc": "6",
-          "value": "6"
-        }, {
-          "desc": "7",
-          "value": "7"
-        }, {
-          "desc": "8",
-          "value": "8"
-        }],
+        "min": 1,
+        "max": 12,
+        "step": 1,
         "desc": "Sets the default visual size of tabs. This setting can be overridden per scene.",
-        "apply": function(val) {} // only adjusts value for new scenes
+        "validate": function(newValue) {
+          var message = "Invalid value";
+          newValue = parseInt(newValue);
+          var valid = !isNaN(newValue);
+          if (valid) {
+            if (newValue < this.min) {
+              valid = false;
+              message = "Value should be greater than or equal to " + this.min;
+            } else if (newValue > this.max) {
+              valid = false;
+              message = "Value should be less than or equal to " + this.max;
+            }
+          }
+          return { valid: valid, message: message }
+        },
+        "apply": function(val) {}, // only adjusts value for new scenes
       }),
       new CSIDESetting({
         "id": "indentspaces",
         "name": "Space Indentation Size",
         "value": "4",
-        "type": "dropdown",
+        "type": "number",
         "cat": "editor",
-        "options": [{
-          "desc": "2",
-          "value": "2"
-        }, {
-          "desc": "3",
-          "value": "3"
-        }, {
-          "desc": "4",
-          "value": "4"
-        }, {
-          "desc": "5",
-          "value": "5"
-        }, {
-          "desc": "6",
-          "value": "6"
-        }, {
-          "desc": "7",
-          "value": "7"
-        }, {
-          "desc": "8",
-          "value": "8"
-        }],
+        "min": 1,
+        "max": 12,
+        "step": 1,
         "desc": "Sets the preferred number of spaces used for indentation. Note that this will only be applied to newly created scenes. This setting can be overridden per scene.",
+        "validate": function(newValue) {
+          var message = "Invalid value";
+          newValue = parseInt(newValue);
+          var valid = !isNaN(newValue);
+          if (valid) {
+            if (newValue < this.min) {
+              valid = false;
+              message = "Value should be greater than or equal to " + this.min;
+            } else if (newValue > this.max) {
+              valid = false;
+              message = "Value should be less than or equal to " + this.max;
+            }
+          }
+          return { valid: valid, message: message }
+        },
         "apply": function(val) {} // only adjusts value for new scenes
       }),
       new CSIDESetting({
         "id": "fontsize",
         "name": "Font Size (px)",
-        "value": "12px",
-        "type": "variable",
+        "value": "12",
+        "type": "number",
         "cat": "editor",
-        "options": [{
-          "desc": "10",
-          "value": "10px"
-        }, {
-          "desc": "12",
-          "value": "12px"
-        }, {
-          "desc": "14",
-          "value": "14px"
-        }, {
-          "desc": "16",
-          "value": "16px"
-        }],
+        "min": 8,
+        "max": 30,
+        "step": 1,
         "desc": "The size of the font in the editor window",
+        "validate": function(newValue) {
+          var message = "Invalid value";
+          newValue = parseInt(newValue);
+          var valid = !isNaN(newValue);
+          if (valid) {
+            if (newValue < this.min) {
+              valid = false;
+              message = "Value should be greater than or equal to " + this.min;
+            } else if (newValue > this.max) {
+              valid = false;
+              message = "Value should be less than or equal to " + this.max;
+            }
+          }
+          return { valid: valid, message: message }
+        },
         "apply": function(val) {
           vseditor.updateOptions({fontSize: val});
         }

source/css/cside.css
@@ -849,9 +849,10 @@ body.night .tab-panel.search #search-controls-wrap {
     float: right;
 }
 
-.tab-panel.settings>input {
-    margin-left: 10px;
-    height: 24px;
+.tab-panel.settings input {
+    color: inherit;
+    width: 100px;
+    margin-top: 5px;
 }
 
 .tab-panel.settings>label {
@@ -1231,7 +1232,6 @@ body.night .scene-error {
 
 .scene-error span {
     font-weight: normal;
-    color: salmon;
     font-size: 12px;
     left: 10px;
     margin-left: 2px;
@@ -1468,6 +1468,18 @@ body.night .project-hover {
 .name-error {
     color: rgb(249, 112, 112) !important;
 }
+
+body.night .validation-error-text {
+    color: salmon;
+}
+
+.validation-error-text {
+    color: #e80b0b;
+    font-size: 12px;
+    margin: 5px 0px 0px 0px;
+}
+
+#d00909
 
 .project-header-selected {
     background-color: rgb(99, 99, 99);

source/index.html
@@ -126,7 +126,7 @@
                                 <button class="colour-marker handle" data-bind="attr: { title: 'Click to colour project ' + getName() }, click: showColours, clickBubble: false, style: { backgroundColor: getMarkColour }"></button>
                                 <!-- <span data-bind="text: getName(), event: { dblclick: rename }, attr: { title: getName }, fadeVisible: beingEdited() ? false : true"></span> -->
                                 <input data-bind="attr: { title: getName }, enableTextInput: beingEdited, textInput: nameInterface, event: { click: isSelected() ? rename : null, dblclick: rename, blur: rename, keyup: rename }, css: { 'name-error': nameInvalid }" class="col-anim"></input>
-                                <div class="scene-error" data-bind="fadeVisible: nameInvalid">
+                                <div class="scene-error validation-error-text" data-bind="fadeVisible: nameInvalid">
                                     <span data-bind="text: getNameErrorMsg"></span>
                                 </div>
                                 <ul class="colour-picker" id="colour-picker" data-bind="fadeVisible: beingColoured, event: { mouseleave: showColours }">
@@ -243,6 +243,12 @@
                                             <!-- ko if: getType() == "dropdown" -->
                                             <br>
                                             <select data-bind="options: getOptions(), optionsText: 'desc', optionsValue: 'value', value: value"></select>
+                                            <!-- /ko -->
+                                            <!-- ko if: getType() == "number" -->
+                                            <br>
+                                            <input type="number" data-bind="attr: { 'min': min, 'max': max, 'step': step }, value: value ">
+                                            <br>
+                                            <p class="validation-error-text" data-bind="fadeVisible: value.isErroneous, text: value.errorMessage"></p>
                                             <!-- /ko -->
                                             <!-- ko if: getType() == "variable" || getType() == "custom" -->
                                             <ul data-bind="foreach: getOptions()">

commit 4a8e262f74ed39b83b45a708cccf728465f3bd21

[bug fix] Indent prompt should use open input
Introduced by:
6384ec968a479fd68d2f0613a254cf17ae292bfd

The indentation settings no longer have
static options to display, so we should
be prompting for numerical input.

source/js/knockoutCSIDE.js
@@ -2690,14 +2690,18 @@ function IDEViewModel() {
     if (!docModel) return;
     _bootboxSelect("Select Indentation Unit", types,
       function(unitOpt) {
-        self.getActiveFile().updateUseSpaces(unitOpt === "Spaces");
-        var sizes = settings.byId("editor", "tabsize").getOptions().map(function(o) { return o.value; });
-        _bootboxSelect("Select Indentation Size", sizes,
-          function(sizeOpt) {
-            self.getActiveFile().updateIndentSize(sizeOpt);
-            bootbox.hideAll();
-          }, docModel.getOptions().tabSize
-        );
+        var useSpaces = (unitOpt === "Spaces");
+        var setting = settings.byId("editor", useSpaces ? "indentspaces" : "tabsize");
+        self.getActiveFile().updateUseSpaces(useSpaces);
+        __promptForInteger(function(value, errmsg) {
+          if (value) {
+            self.getActiveFile().updateIndentSize(value);
+          } else if (errmsg) {
+            bootbox.alert("Invalid value: " + errmsg);
+          } else {
+            // user cancelled / didn't give a value
+          }
+        }, "Indentation Size", setting.getValue(), { max: setting.max, min: setting.min });
       }, types[Number(docModel.getOptions().insertSpaces)]
     );
   }
@@ -4780,6 +4784,35 @@ function IDEViewModel() {
         }
         else {
           callback(__trimWhitespace(str));
+        }
+      }
+    });
+  }
+
+  function __promptForInteger(callback, title, placeholder, options) {
+    // options = { min, max }
+    bootbox.prompt({
+      title: title || "Please enter text",
+      value: placeholder || "",
+      callback: function(input) {
+        newVal = Number(input);
+        isInteger = Number.isInteger(newVal);
+        if (!input) {
+          callback("", (input === null) ? "" : "no input"); // cancelled / empty
+        } else if (isNaN(newVal)) {
+          callback("", "not a number");
+        } else if (!Number.isInteger(newVal)) {
+          callback("", "not an integer");
+        } else {
+          if (options) {
+            if (newVal > options.max) {
+              callback("", newVal + " should be less than or equal to " + options.max);
+            } else if (newVal < options.min) {
+              callback("", newVal + " should be greater than or equal to " + options.min);
+            } else {
+              callback(newVal);
+            }
+          }
         }
       }
     });