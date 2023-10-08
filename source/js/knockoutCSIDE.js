ChoicescriptIDE/main

commit eadbab506ba26791b0d14c8de66663ecaccee308

Don't generate issues for unopened files

source/js/knockoutCSIDE.js
@@ -4613,13 +4613,15 @@ function IDEViewModel(platform, versions, userDetails, appPath, db) {
         var project = getProject(getProjectPath(uri[i].path));
         if (project) {
           var markers = monaco.editor.getModelMarkers({resource: uri[i], take: 500 });
-          var file = project.getFiles().find(function(f) {
+          var fileOpen = project.getFiles().find(function(f) {
             return f.getPath() === uri[i].path;
           });
-          var newIssues = markers.map(function(m) {
-            return new CSIDEIssue({ file: file, lineNum: m.startLineNumber, desc: m.message, severity: m.severity });
-          });
-          if (file) file.setMarkerIssues(newIssues);
+          if (fileOpen) {
+            var newIssues = markers.map(function(m) {
+              return new CSIDEIssue({ file: file, lineNum: m.startLineNumber, desc: m.message, severity: m.severity });
+            });
+            if (file) file.setMarkerIssues(newIssues);
+          }
         }
       }
     });

commit a96f852e67f6b6def010e0f0363d9c442c2d863d

Fix issue log regression
eadbab506ba26791b0d14c8de66663ecaccee308
introduced an issue caused by variable
name shadowing where marker issues were
always being logged against the same file.

source/js/knockoutCSIDE.js
@@ -4613,14 +4613,14 @@ function IDEViewModel(platform, versions, userDetails, appPath, db) {
         var project = getProject(getProjectPath(uri[i].path));
         if (project) {
           var markers = monaco.editor.getModelMarkers({resource: uri[i], take: 500 });
-          var fileOpen = project.getFiles().find(function(f) {
+          var f = project.getFiles().find(function(f) {
             return f.getPath() === uri[i].path;
           });
-          if (fileOpen) {
+          if (f) {
             var newIssues = markers.map(function(m) {
-              return new CSIDEIssue({ file: file, lineNum: m.startLineNumber, desc: m.message, severity: m.severity });
+              return new CSIDEIssue({ file: f, lineNum: m.startLineNumber, desc: m.message, severity: m.severity });
             });
-            if (file) file.setMarkerIssues(newIssues);
+            if (f) f.setMarkerIssues(newIssues);
           }
         }
       }